4ba6a4daa2ab9c516ee3faac3c229f4a7f997c8f
jacobnoel101 <noel2jr@dukes.jmu.edu>
2025-11-13 21:31:41 -0500

    bowlingpin logic
    added bowlingpin logic and visual.

--- src/bowlingSprites/BowlingBall.java
+++ src/bowlingSprites/BowlingBall.java
@@ -3,54 +3,30 @@ package bowlingSprites;
 import java.awt.*;
 import java.awt.geom.*;
 import visual.dynamic.described.DescribedSprite;
+import visual.dynamic.described.RuleBasedSprite;
 import visual.statik.described.*;
 
-public class BowlingBall extends DescribedSprite
+public class BowlingBall extends RuleBasedSprite
 {
   private final int radius = 25; // ball size
   private double cx = 500.0; // x pos on screen
   private double cy = 650.0; // y pos on screen
+  double maxX;
+  double maxY;
+  Double speed;
+  
 
-  public BowlingBall()
+  public BowlingBall(TransformableContent content, double width, double height, Double speed) 
   {
-    // create agg content to hold ball and finger holes
-    AggregateContent agg = new AggregateContent();
-    Content ballShape = new Content(); // main circle
-
-    // create shape of the ball and set color to blue
-    ballShape.setShape(new Ellipse2D.Double(-radius, -radius, radius * 2, radius * 2));
-    ballShape.setPaint(Color.BLUE);
-    agg.add(ballShape); // add circle to agg content
-
-    // add color inside of circle for texture
-    Content highlight = new Content();
-    highlight
-        .setShape(new Ellipse2D.Double(-radius * 0.7, -radius * 0.8, radius * 1.4, radius * 1.4));
-    highlight.setPaint(new Color(255, 255, 255, 60));
-    agg.add(highlight);
-
-    double holeR = 2.25; // size of finger holes
-
-    // first finger hole
-    Content h1 = new Content();
-    h1.setShape(new Ellipse2D.Double(-holeR - 5, -radius / 2.0, holeR * 2, holeR * 2));
-    h1.setPaint(Color.BLACK);
-    agg.add(h1);
-    // second finger hole (mirrored to first)
-    Content h2 = new Content();
-    h2.setShape(new Ellipse2D.Double(5 - holeR, -radius / 2.0, holeR * 2, holeR * 2));
-    h2.setPaint(Color.BLACK);
-    agg.add(h2);
-    // third finger hole (centered lower between h1 and h2)
-    Content h3 = new Content();
-    h3.setShape(new Ellipse2D.Double(-holeR, -radius / 2.0 + 10, holeR * 2, holeR * 2));
-    h3.setPaint(Color.BLACK);
-    agg.add(h3);
-
-    addKeyTime(0, new Point2D.Double(0, 0), 0.0, 1.0, agg);
-    addKeyTime(1, new Point2D.Double(0, 0), 0.0, 1.0, agg);
-    setLocation(cx, cy); // place on screen
-    setEndState(DescribedSprite.REMAIN);
+    super(content);
+    this.maxX = width;
+    this.maxY = height;
+    if (speed == null) {
+      this.speed = 10.0;
+    } else {
+      this.speed = speed;
+    }
+    setLocation(maxX, maxY);
   }
 
   @Override
--- src/bowlingSprites/BowlingPin.java
+++ src/bowlingSprites/BowlingPin.java
@@ -1,54 +1,96 @@
 package bowlingSprites;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Point2D;
+import java.awt.Polygon;
 import java.awt.geom.Rectangle2D;
-import java.awt.geom.Rectangle2D.Double;
 
 import visual.dynamic.described.*;
-import visual.statik.described.AggregateContent;
 import visual.statik.described.Content;
+import visual.statik.described.TransformableContent;
 
-public class BowlingPin extends DescribedSprite
+public class BowlingPin extends RuleBasedSprite
 {
+  private int row;
   Content pinContent;
-  public BowlingPin() {
-    Rectangle2D shape = new Rectangle2D.Double(0,0, 500, 350);
-    
-    Rectangle2D bounds = shape.getBounds2D();
-    AffineTransform at = new AffineTransform();
-    Shape centeredShape = at.createTransformedShape(shape);
-    Point2D center = new Point2D.Double(bounds.getCenterX(), bounds.getCenterY());
 
-                                                           // 255)
-    // Create Content for the first key time
-    Content cloudShapeContent = new Content(centeredShape, Color.black, Color.black,
-        new BasicStroke(1.0f));
 
-    // Wrap it in AggregateContent
-    AggregateContent aggregateCloudContent = new AggregateContent();
-    aggregateCloudContent.add(cloudShapeContent);
-    aggregateCloudContent.setLocation(100, 100);
-    Point2D location = new Point2D.Double(x, y);
-    addKeyTime(0, center, null, 1.0, aggregateCloudContent);
-
-    
-
+  public BowlingPin(TransformableContent content, int row)
+  {
+    super(content);
+    this.row = row;
   }
-  
-  
+
   @Override
   public void render(Graphics g)
   {
-    super.render(g);
-    Graphics2D g2 = (Graphics2D) g;
-    pinContent.render(g2);
+    double rx, ry;
+    Polygon topPin = new Polygon();
+    Rectangle2D bounds;
+    visual.statik.TransformableContent tc;
+    if (visible)
+    {
+      tc = getContent();
+      if (tc != null)
+      {
+        // Find the point to rotate around
+        if (rotationPoint)
+        {
+          rx = rotationX;
+          ry = rotationY;
+        }
+        else
+        {
+          bounds = tc.getBounds2D(false);
+          rx = bounds.getWidth() / 2.0;
+          ry = bounds.getHeight() / 2.0;
+        }
+        // Transform
+        topPin.addPoint(0, 0);
+        topPin.addPoint(30, 0);
+        topPin.addPoint(30 - 5, -5); // x , y  -> reduce y to make top smaller
+        topPin.addPoint(0 + 5, -5);
+
+        TransformableContent topPinContent = new Content(topPin, Color.black, Color.gray, null);
+        topPinContent.setLocation(x, y);
+        topPinContent.setRotation(angle, rx, ry);
+        topPinContent.setScale(scaleX, scaleY);
+        tc.setLocation(x, y);
+        tc.setRotation(angle, rx, ry);
+        tc.setScale(scaleX, scaleY);
+        // Render
+        tc.render(g);
+        topPinContent.render(g);
+      }
+    }
+
   }
-  
+  // setLocation(maxX, maxY);
+  // Rectangle2D shape = new Rectangle2D.Double(0,0, 500, 350);
+  //
+  // Rectangle2D bounds = shape.getBounds2D();
+  // AffineTransform at = new AffineTransform();
+  // Shape centeredShape = at.createTransformedShape(shape);
+  // Point2D center = new Point2D.Double(bounds.getCenterX(), bounds.getCenterY());
+  //
+  // // 255)
+  // // Create Content for the first key time
+  // Content cloudShapeContent = new Content(centeredShape, Color.black, Color.black,
+  // new BasicStroke(1.0f));
+  //
+  // // Wrap it in AggregateContent
+  // AggregateContent aggregateCloudContent = new AggregateContent();
+  // aggregateCloudContent.add(cloudShapeContent);
+  // aggregateCloudContent.setLocation(100, 100);
+  // Point2D location = new Point2D.Double(x, y);
+  // //addKeyTime(0, center, null, 1.0, aggregateCloudContent);
+
+  @Override
+  public void handleTick(int arg0)
+  {
+    // TODO Auto-generated method stub
+
+  }
+
 }
